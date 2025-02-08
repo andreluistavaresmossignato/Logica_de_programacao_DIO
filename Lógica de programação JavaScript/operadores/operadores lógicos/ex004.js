095ae02faa2e9a40c079bf93be5015d667ff3a05
André Luís Tavares Mossignato <andreluis.mossignato@gmail.com>
2025-02-08 14:36:28 -0300

    Adicionei  o NOT ( ! )

---
+++ Lógica de programação JavaScript/operadores/operadores lógicos/ex004.js
@@ -0,0 +1,5 @@
+// NOT ( ! ) - nega uma afirmação
+
+let tempo = "chuva"
+let resultado = tempo !== "chuva" // false
+console.log(!resultado) // volta para true
---
+++ Lógica de programação JavaScript/operadores/operadores lógicos/ex005.js
@@ -0,0 +1,7 @@
+// NOT ( ! ) - nega uma afirmação
+
+let tempo = "chuva"
+let horario = 8
+
+let resultado = !((tempo !== "chuva") && (horario > 6)) // inverte a polaridade
+console.log(resultado)
